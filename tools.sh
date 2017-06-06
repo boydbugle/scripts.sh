c2c492e06159a3c9d519f8fe5be51d29adae0ea5
ndongA <boydbugle@gmai.com>
2017-06-07 00:34:33 +0300

    install chrome

--- tools.sh
+++ tools.sh
@@ -15,3 +15,19 @@ sudo apt update
 sudo apt install vlc
 
 echo "vlc installation complete"
+
+echo "install chrome; add key"
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+
+echo "set repo"
+
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+echo "install and update"
+
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+echo "chrome complete"
+
